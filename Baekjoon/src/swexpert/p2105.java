d9bd465e5ca5cfba765a5abbdcd48b859313bfac
Seongyong <oh9301@naver.com>
2018-03-08 14:43:58 +0900

    디저트 카페 / 개선(SWEXPERT 모의 SW 역량테스트)

--- Baekjoon/src/swexpert/p2105.java
+++ Baekjoon/src/swexpert/p2105.java
@@ -3,27 +3,23 @@ package swexpert;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-//2000MS 비효율적..
 public class p2105 {
 
 	static int answer;
 	static int[][] map;
 	static int N;
-	static int startX;
-	static int startY;
+	static int startX, startY, dirEnd;
 	static int ptResult;
-	static int[] posX = { -1, 1, -1, 1 };
-	static int[] posY = { 1, 1, -1, -1 };
+	static int[] posX = { 1, 1, -1, -1 };
+	static int[] posY = { -1, 1, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
-			answer = 0;
+			answer = -1;
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
 			for (int i = 0; i < N; i++) {
@@ -33,52 +29,43 @@ public class p2105 {
 			}
 
 			calcMaxDessert();
-			if (answer == 0)
-				answer = -1;
 			System.out.println("#" + (t + 1) + " " + answer);
 		}
 	}
 
 	public static void calcMaxDessert() {
-		Set<Integer> vSet;
-		Set<Integer> dirSet;
+		boolean[] visit = new boolean[101];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				vSet = new HashSet<>();
-				dirSet = new HashSet<>();
-				ptResult = 0;
-				startX = i;
-				startY = j;
-				searchDessertPath(i, j, vSet, dirSet, 0, 0);
-				answer = Math.max(ptResult, answer);
+				ptResult = -1;
+				for (int k = 0; k < 4; k++) {
+					startX = i;	startY = j;
+					dirEnd = k+3;
+					searchDessertPath(k, i, j, 0, visit);
+					answer = Math.max(ptResult, answer);
+				}
 			}
 		}
 	}
 
-	public static void searchDessertPath(int i, int j, Set visit, Set dirSet, int len, int prevDis) {
-		visit.add(map[i][j]);
-		if (len != 0 && i == startX && j == startY) {
-			if (dirSet.size() == 4) {
-				ptResult = Math.max(ptResult, len);
-				return;
-			}
+	public static void searchDessertPath(int dir, int i, int j, int len, boolean[] visit) {
+		if (dir == dirEnd && i == startX && j == startY) {
+			ptResult = Math.max(ptResult, len);
 			return;
 		}
-		for (int k = 1; k <= 4; k++) {
-			int ni = i + posX[k - 1];
-			int nj = j + posY[k - 1];
-			if (ni >= 0 && nj >= 0 && ni < N && nj < N) {
-				if (!visit.contains(map[ni][nj]) || (ni == startX && nj == startY)) {
-					if (!dirSet.contains(k)) {
-						dirSet.add(k);
-						searchDessertPath(ni, nj, visit, dirSet, len + 1, k);
-						dirSet.remove(k);
-					} else if (prevDis == k)
-						searchDessertPath(ni, nj, visit, dirSet, len + 1, k);
-				}
-			}
-		}
-		visit.remove(map[i][j]);
+		if(visit[map[i][j]])
+			return;
+		else
+			visit[map[i][j]] = true;
+		int ni = i + posX[dir%4];
+		int nj = j + posY[dir%4];
+		if (ni >= 0 && nj >= 0 && ni < N && nj < N)
+			searchDessertPath(dir, ni, nj, len + 1, visit);
+		int ni2 = i + posX[(dir+1)%4];
+		int nj2 = j + posY[(dir+1)%4];
+		if (dir <= dirEnd-1 && ni2 >= 0 && nj2 >= 0 && ni2 < N && nj2 < N)
+			searchDessertPath(dir+1, ni2, nj2, len + 1, visit);
+		visit[map[i][j]] = false;
 	}
 
 }
