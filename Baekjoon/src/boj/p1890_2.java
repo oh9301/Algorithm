9f6bb138baf53481c6a983f14359e51a73f7a7dc
Seongyong <oh9301@naver.com>
2018-03-12 14:06:29 +0900

    점프 (BFS)

--- Baekjoon/src/boj/p1890_2.java
+++ Baekjoon/src/boj/p1890_2.java
@@ -5,6 +5,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 //bfs + memo
+class Point{
+	int x;
+	int y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+}
+
 public class p1890_2 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
