924d43f7ac1a8e8e4d20c8c3251af812f1271e20
Seongyong <oh9301@naver.com>
2018-03-12 13:57:31 +0900

    세수의 합

---
+++ Baekjoon/src/boj/p2295.java
@@ -0,0 +1,35 @@
+package boj;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class p2295 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N; j++) {
+				for (int k = j; k < N; k++) {
+					if(arr[i] + arr[j] + arr[k] > arr[N-1])
+						break;
+					if(Arrays.binarySearch(arr, arr[i] + arr[j] + arr[k]) >= 0)
+						max = (max < arr[i] + arr[j] + arr[k]) ? arr[i] + arr[j] + arr[k] : max; 
+				}
+			}
+		}
+		System.out.println(max);
+		
+	}
+
+}
+
