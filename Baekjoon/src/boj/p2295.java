7c3343926b23436beb8ca6508e5f11d8d7d8b679
Seongyong <oh9301@naver.com>
2018-03-21 15:24:16 +0900

    세수의 합 (STL)

--- Baekjoon/src/boj/p2295.java
+++ Baekjoon/src/boj/p2295.java
@@ -1,34 +1,37 @@
 package boj;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.HashMap;
 
 public class p2295 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+		HashMap<Integer, Integer> chkMap = new HashMap<>();
+		for (int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+			chkMap.put(arr[i], 1);
 		}
 		
 		Arrays.sort(arr);
-		int max = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = i; j < N; j++) {
-				for (int k = j; k < N; k++) {
-					if(arr[i] + arr[j] + arr[k] > arr[N-1])
+		for (int i = N-1; i >= 0; i--) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					if(arr[i] - arr[j] - arr[k] < 0)
 						break;
-					if(Arrays.binarySearch(arr, arr[i] + arr[j] + arr[k]) >= 0)
-						max = (max < arr[i] + arr[j] + arr[k]) ? arr[i] + arr[j] + arr[k] : max; 
+					if(chkMap.get(arr[i] - arr[j] - arr[k]) != null){
+						System.out.println(arr[i]);
+						return;
+					}
 				}
 			}
 		}
-		System.out.println(max);
-		
 	}
 
 }
