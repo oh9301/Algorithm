c30081acd755bf3a57a533061b5fe93f49fd01d0
Seongyong <oh9301@naver.com>
2018-03-26 15:17:49 +0900

    수 고르기 (STL)

--- Baekjoon/src/boj/p2230.java
+++ Baekjoon/src/boj/p2230.java
@@ -1,20 +1,23 @@
 package boj;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class p2230 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		int[] iArr = new int[N];
 		int min = 2000000000;
 		
-		for (int i = 0; i < iArr.length; i++) {
-			iArr[i] = sc.nextInt();
-		}
+		for (int i = 0; i < iArr.length; i++)
+			iArr[i] = Integer.parseInt(br.readLine());
 		
 		Arrays.sort(iArr);
 		
