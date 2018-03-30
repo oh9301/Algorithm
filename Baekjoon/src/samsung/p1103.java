f7b702cb3a6804da24245407f906572fd6af66d9
Seongyong <oh9301@naver.com>
2018-03-30 21:21:59 +0900

    게임 (삼성 SW역량 대비 / 수정중)

---
+++ Baekjoon/src/samsung/p1103.java
@@ -0,0 +1,76 @@
+package samsung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Coin{
+	int x;
+	int y;
+	int count;
+	public Coin(int x, int y, int count) {
+		this.x = x;
+		this.y = y;
+		this.count = count;
+	}
+	
+}
+
+public class p1103 {
+	
+	static int N, M, answer;
+	static int[][] map;
+	static int[] posi = {-1, 0, 1, 0};
+	static int[] posj = {0, -1, 0, 1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String inp = br.readLine();
+			for (int j = 0; j < M; j++) {
+				char c = inp.charAt(j);
+				if(c == 'H')
+					map[i][j] = 0;
+				else
+					map[i][j] = c - '0';
+			}
+		}
+		BFS();
+		System.out.println(answer);
+	}
+	
+	public static void BFS(){
+		boolean[][] visited = new boolean[N][M];
+		Queue<Coin> queue = new LinkedList<>();
+		queue.add(new Coin(0, 0, 1));
+		visited[0][0] = true;
+		while(!queue.isEmpty()){
+			Coin c = queue.poll();
+			for (int k = 0; k < 4; k++) {
+				int ci = c.x + posi[k]*map[c.x][c.y];
+				int cj = c.y + posj[k]*map[c.x][c.y];
+				if(ci < 0 || cj < 0 || ci >= N || cj >= M || map[ci][cj] == 0){
+					answer = Math.max(answer, c.count);
+					continue;
+				}
+				if(!visited[ci][cj]){
+					visited[ci][cj] = true;
+					queue.add(new Coin(ci, cj, c.count + 1));
+				} else{
+					System.out.println(c.x + " " + c.y);
+					System.out.println(ci + " " + cj);
+					answer = -1;
+					return;
+				}
+			}
+		}
+	}
+
+}
