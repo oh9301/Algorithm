d210b62ffc1db298f466347dd97f0a07f6e74327
Seongyong <oh9301@naver.com>
2018-04-12 21:07:48 +0900

    미로2 (D4)

---
+++ Baekjoon/src/swexpert/p1227.java
@@ -0,0 +1,59 @@
+package swexpert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MP{
+	int i;
+	int j;
+	
+	public MP(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+}
+
+public class p1227 {
+	
+	static int si, sj, ei, ej;
+	static int answer;
+	static char[][] map;
+	static boolean[][] visited;
+	static int[] posi = {-1, 0, 1, 0};
+	static int[] posj = {0, -1, 0, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int t = 1; t <= 10; t++) {
+			answer = 0;
+			int T = Integer.parseInt(br.readLine());
+			map = new char[100][100];
+			visited = new boolean[100][100];
+			for (int i = 0; i < 100; i++) {
+				String input = br.readLine();
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = input.charAt(j);
+					if(map[i][j] == '2'){
+						si = i;
+						sj = j;
+					}else if(map[i][j] == '3'){
+						ei = i;
+						ej = j;
+					}
+				}
+			}
+			bfs();
+			System.out.println("#"+T+" "+answer);
+		}
+	}
+	
+	public static void bfs(){
+		Queue<MP> queue = new LinkedList<>();
+		queue.add(new MP(si, sj));
+		
+	}
+}
